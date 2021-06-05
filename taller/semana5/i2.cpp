0305d728604d32ba408ace0ab1b95c3147bb9edc
negamartin <martin.andrighetti@gmail.com>
2021-06-05 17:50:10 -0400

    Knights tour

--- taller/semana5/i2.cpp
+++ taller/semana5/i2.cpp
@@ -10,13 +10,12 @@ using namespace std;
 #define invrep2(i, n) invrep3(i, 0, n)
 #define invrep(...) REPGET(__VA_ARGS__, invrep3, invrep2)(__VA_ARGS__)
 
-int8_t board[8][8];
 int16_t hsum[8];
 uint8_t hrem[8];
 int16_t vsum[8];
 uint8_t vrem[8];
 pair<int8_t, int8_t> pos[64];
-uint64_t possible[64];
+uint64_t unknown;
 
 uint64_t shift(uint64_t b, int x, int y) {
     if (y < 0) {
@@ -41,33 +40,39 @@ uint64_t step_board(uint64_t b) {
 }
 uint64_t piece_at(int x, int y) { return ((uint64_t)1) << (x + y * 8); }
 
+// board is -1 at (x, y)
 bool search(int num, int x, int y) {
     if (x < 0 || x >= 8 || y < 0 || y >= 8) return false;
-    if ((possible[num - 1] & piece_at(x, y)) == 0) return false;
-    if (pos[num - 1].first == -1) {
-        if (board[x][y] != -1) return false;
-        if (vsum[x] + vrem[x] * (vrem[x] + num - 1) > 260 ||
-            hsum[y] + hrem[y] * (hrem[y] + num - 1) > 260) {
+    if ((unknown & piece_at(x, y)) == 0) {
+        if (pos[num - 1].first == -1) {
             return false;
+        } else {
+            if (pos[num - 1].first == x && pos[num - 1].second == y) {
+                num += 1;
+                while (num <= 64 && pos[num - 1].first != -1) {
+                    num += 1;
+                }
+            } else {
+                return false;
+            }
         }
-        if (vsum[x] + num + (vrem[x] - 1) * (66 - vrem[x]) < 260 ||
-            hsum[y] + num + (hrem[y] - 1) * (66 - hrem[y]) < 260) {
-            return false;
+    }
+
+    bool ok = true;
+    unknown &= ~piece_at(x, y);
+    pos[num - 1] = {x, y};
+    vsum[x] += num;
+    vrem[x] -= 1;
+    hsum[y] += num;
+    hrem[y] -= 1;
+    rep(i, 8) {
+        if (vsum[i] + vrem[i] * (vrem[i] + num) > 260 ||
+            hsum[i] + hrem[i] * (hrem[i] + num) > 260) {
+            ok = false;
+            break;
         }
-        board[x][y] = num;
-        vsum[x] += num;
-        vrem[x] -= 1;
-        hsum[y] += num;
-        hrem[y] -= 1;
     }
-    int nnum = num + 1;
-    int nx = x, ny = y;
-    while (nnum <= 64 && pos[nnum - 1].first != -1) {
-        nx = pos[nnum - 1].first;
-        ny = pos[nnum - 1].second;
-        nnum += 1;
-    }
-    if (nnum > 64) {
+    if (num >= 64) {
         bool exact = true;
         rep(i, 8) {
             if (vsum[i] != 260 || hsum[i] != 260) {
@@ -76,23 +81,32 @@ bool search(int num, int x, int y) {
             }
         }
         if (exact) return true;
-    } else {
-        bool done =
-            search(nnum, nx + 2, ny - 1) || search(nnum, nx + 2, ny + 1) ||
-            search(nnum, nx - 2, ny - 1) || search(nnum, nx - 2, ny + 1) ||
-            search(nnum, nx + 1, ny - 2) || search(nnum, nx + 1, ny + 2) ||
-            search(nnum, nx - 1, ny - 2) || search(nnum, nx - 1, ny + 2);
-        if (done) {
-            return true;
+    }
+    if (ok) {
+        int nknown = num;
+        uint64_t canreach = piece_at(x, y);
+        while (nknown <= 64 && pos[nknown - 1].first == -1) {
+            canreach = unknown & step_board(canreach);
+            nknown += 1;
+        }
+        if (nknown > 64 || (canreach & piece_at(pos[nknown - 1].first,
+                                                pos[nknown - 1].second)) != 0) {
+            bool done =
+                search(nnum, nx + 2, ny - 1) || search(nnum, nx + 2, ny + 1) ||
+                search(nnum, nx - 2, ny - 1) || search(nnum, nx - 2, ny + 1) ||
+                search(nnum, nx + 1, ny - 2) || search(nnum, nx + 1, ny + 2) ||
+                search(nnum, nx - 1, ny - 2) || search(nnum, nx - 1, ny + 2);
+            if (done) {
+                return true;
+            }
         }
     }
-    if (pos[num - 1].first == -1) {
-        board[x][y] = -1;
-        vsum[x] -= num;
-        vrem[x] += 1;
-        hsum[y] -= num;
-        hrem[y] += 1;
-    }
+    unknown |= piece_at(x, y);
+    pos[num - 1].first = -1;
+    vsum[x] -= num;
+    vrem[x] += 1;
+    hsum[y] -= num;
+    hrem[y] += 1;
     return false;
 }
 
@@ -107,42 +121,25 @@ int main() {
         vsum[i] = 0;
         hsum[i] = 0;
     }
-    uint64_t availbase = ~0ull;
+    unknown = ~0ull;
     rep(y, 8) {
         rep(x, 8) {
             int v;
             cin >> v;
-            board[x][y] = v;
-            if (v > 0) {
+            if (v >= 1) {
                 vsum[x] += v;
                 vrem[x] -= 1;
                 hsum[y] += v;
                 hrem[y] -= 1;
                 pos[v - 1] = {x, y};
-                availbase &= ~piece_at(x, y);
+                unknown &= ~piece_at(x, y);
             }
         }
-    }
-    uint64_t avail = availbase;
-    invrep(i, 64) {
-        if (pos[i].first == -1) {
-            avail = availbase & step_board(avail);
-        } else {
-            avail = piece_at(pos[i].first, pos[i].second);
-        }
-        cout << "num " << (i + 1) << ":" << endl;
-        uint64_t b = avail;
-        rep(y, 8) {
-            rep(x, 8) {
-                bool p = (b & piece_at(x, y)) != 0;
-                cout << p << " ";
-            }
-            cout << endl;
-        }
-        possible[i] = avail;
     }
 
     search(1, pos[0].first, pos[0].second);
+    uint8_t board[8][8];
+    rep(i, 64) { board[pos[i].first][pos[i].second] = i + 1; }
     rep(y, 8) {
         rep(x, 8) {
             if (x != 0) cout << " ";
