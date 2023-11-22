8a2ee7d9014ec82126f70882517f3ac827d0abbb
kovaxis <martin.andrighetti@gmail.com>
2023-11-22 17:41:56 -0300

    format template

--- material/template/e.cpp
+++ material/template/e.cpp
@@ -9,14 +9,12 @@ typedef long long ll;
 #define invrepx(i, a, b) for (int i = b - 1; i >= a; i--)
 #define invrep(i, n) invrepx(i, 0, n)
 
-int main()
-{
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
     int TC;
     cin >> TC;
-    while (TC--)
-    {
+    while (TC--) {
     }
 }
